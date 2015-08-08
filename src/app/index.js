36c2769c1ca5e4bdd522443204d27f12dc19d5bb
Jon Rutishauser <jrutishauser@gmail.com>
2015-08-08 18:29:21 -0400

    routes work properly

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,54 @@
-'use strict';
+"use strict";
 
-angular.module('church', ['ngAnimate', 'ngSanitize', 'ngResource', 'ui.router','ui.bootstrap'])
+angular.module("church", ["ngAnimate", "ngSanitize", "ngResource", "ui.router","ui.bootstrap"])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+      .state("home", {
+        url: "/",
+        templateUrl: "app/main/main.html",
+        controller: "MainCtrl"
+      })
+      .state("facilities", {
+        url: "/facilities",
+        templateUrl: "app/facilities/facilities.html",
+        controller: "FacilitiesCtrl"
+      })
+      .state("give", {
+        url: "/give",
+        templateUrl: "app/give/give.html",
+        controller: "GiveCtrl"
+      })
+      .state("learn", {
+        url: "/learn",
+        templateUrl: "app/learn/learn.html",
+        controller: "LearnCtrl"
+      })
+      .state("ministries", {
+        url: "/ministries",
+        templateUrl: "app/ministries/ministries.html",
+        controller: "MinistriesCtrl"
+      })
+      .state("opportunities", {
+        url: "/opportunities",
+        templateUrl: "app/opportunities/opportunities.html",
+        controller: "OpportunitiesCtrl"
+      })
+      .state("plan", {
+        url: "/plan",
+        templateUrl: "app/plan/plan.html",
+        controller: "PlanCtrl"
+      })
+      .state("spiritual", {
+        url: "/spiritual",
+        templateUrl: "app/spiritual/spiritual.html",
+        controller: "SpiritualCtrl"
+      })
+      .state("thriftshop", {
+        url: "/thriftshop",
+        templateUrl: "app/thriftshop/thriftshop.html",
+        controller: "ThriftshopCtrl"
       })
 	  ;//end of routes
-
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise("/");
   })
 ;
