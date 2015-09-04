9f41da1142a477c2a59aa67b63b21243fd98720c
Jon Rutishauser <jrutishauser@gmail.com>
2015-09-04 01:16:36 -0400

    by usng angular on this site the href go to ID interfered with routing and would not work, wrote function in controller so that an ID can be scrolled to, added some resized images for the main slider and added content for the ministries page

--- src/app/ministries/ministries.controller.js
+++ src/app/ministries/ministries.controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('church')
-  .controller('MinistriesCtrl', function ($scope) {
+  .controller('MinistriesCtrl', function ($scope, $location, $anchorScroll) {
 		  console.log('ministries');
+		  $scope.scrollTo = function(id) {
+		      $location.hash(id);
+		      console.log($location.hash());
+		      $anchorScroll();
+		    };
   });
