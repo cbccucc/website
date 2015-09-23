bb3611a3e60981b60b7a46c220c7048c5f5e0086
Jon Rutishauser <jrutishauser@gmail.com>
2015-09-23 19:50:18 -0400

    wfellowship page

--- src/app/womensfellowship/womensfellowship.controller.js
+++ src/app/womensfellowship/womensfellowship.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('church')
-  .controller('WomensfellowshipCtrl', function ($scope) {
-		  console.log('womens fellowship');
+  .controller('WomensfellowshipCtrl', function ($scope, $location, $anchorScroll) {
+		  $scope.scrollTo = function(id) {
+		      $location.hash(id);
+		      console.log($location.hash());
+		      $anchorScroll();
+		    };
   });
