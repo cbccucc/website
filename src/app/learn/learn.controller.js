6bcc895d311c16c30df70b2ed9040d0c38b26a41
Jon Rutishauser <jrutishauser@gmail.com>
2015-09-23 23:54:57 -0400

    added images to the wf page and thrift page also added heading links and such to other pages

--- src/app/learn/learn.controller.js
+++ src/app/learn/learn.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('church')
-  .controller('LearnCtrl', function ($scope) {
-		  console.log('learn');
+  .controller('LearnCtrl', function ($scope, $location, $anchorScroll) {
+		  $scope.scrollTo = function(id) {
+		      $location.hash(id);
+		      console.log($location.hash());
+		      $anchorScroll();
+		    };
   });
