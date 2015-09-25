2e34e7f2c229d9c63f8e8d8b8b47c956962d6f40
Jon Rutishauser <jrutishauser@gmail.com>
2015-09-24 23:14:14 -0400

    added facilities page

--- src/app/facilities/facilities.controller.js
+++ src/app/facilities/facilities.controller.js
@@ -1,7 +1,11 @@
 'use strict';
 
 angular.module('church')
-  .controller('FacilitiesCtrl', function ($scope) {
-		  console.log('facilities')
+  .controller('FacilitiesCtrl', function ($scope, $location, $anchorScroll) {
+		  $scope.scrollTo = function(id) {
+		      $location.hash(id);
+		      console.log($location.hash());
+		      $anchorScroll();
+		    };
   });
 
